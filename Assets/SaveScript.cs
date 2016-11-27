e930f9b3d7c8349a2394a68263eeed0f11e34df0
大塚勇人 <ootuka0313@yahoo.co.jp>
2016-11-27 12:49:53 +0900

    SaveScript

---
+++ Assets/SaveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using  UnityEngine.UI;
+using System.Collections;
+
+public class SaveScript : MonoBehaviour
+{
+    private string str;
+    public InputField inputField;
+    public Text text;
+
+    public void SaveText()
+    {
+        str = inputField.text;
+        text.text = str;
+        inputField.text = "";
+    }
+}
